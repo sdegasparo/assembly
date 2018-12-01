1099fff50ce300f261964d1ab5bd1725949f8960
zeroplexer <41778593+zeroplexer@users.noreply.github.com>
2018-12-01 15:04:42 +0100

    Add files via upload

--- base32enc.asm
+++ base32enc.asm
@@ -61,8 +61,8 @@ prepareRegister:
 	xor rbx, rbx		; rbx
 	xor rcx, rcx		; rcx
 	xor rdx, rdx		; rdx
-	xor r8, r8		; r8 zaehler fuer durchlauf
-	xor r9, r9		; r9 Output
+	xor r8, r8		;
+	xor r9, r9		; 
 	xor r11, r11		; Ausgabe groesse counter speichern
 
 T:				; Debug
@@ -73,14 +73,14 @@ shift:
 	mov bx, [rsi]		; Byte 1 + 2 in bx kopieren
 	shr bx, 3		; 3 nach rechts shiften, damit die ersten 5 Bits verarbeitet werden. 1. Zeichen gefunden.
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutput		; Base32 Charakter ausgeben
+	call addOutputFirst	; Base32 Charakter ausgeben
 	
 	
 	;; 2. 5 bit
 	shl bx, 8		;
 	shr bx, 3		; 2. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutput		; Base32 Charakter ausgeben
+	call addOutputSecond	; Base32 Charakter ausgeben
 
 	;; Byte 2 + 3 einlesen
 	mov bx, [rsi]		; Byte 2 + 3 in bx kopieren
@@ -88,14 +88,14 @@ shift:
 	;; 3. 5 bit
 	shl bx, 2
 	shr bx, 3		; 3. Zeichen gefunden
-	mov bh, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutput		; Base32 Charakter ausgeben
+	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
+	call addOutputFirst	; Base32 Charakter ausgeben
 
 	;; 4. 5 bit
 	shl bx, 8
 	shr bx, 3		; 4. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutput		; Base32 Charakter ausgeben
+	call addOutputSecond	; Base32 Charakter ausgeben
 
 	;; Byte 3 + 4 einlesen
 	mov bx, [rsi]		; Byte 3 + 4 einlesen
@@ -104,13 +104,13 @@ shift:
 	shl bx, 4
 	shr bx, 3		; 5. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutput		; Base32 Charakter ausgeben
+	call addOutputFirst	; Base32 Charakter ausgeben
 
 	;; 6. 5 bit
 	shl bx, 8
 	shr bx, 3		; 6. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutput		; Base32 Charakter ausgeben
+	call addOutputSecond	; Base32 Charakter ausgeben
 
 	;; Byte 4 + 5 einlesen
 	mov bx, [rsi]		; Byte 4 + 5 einlesen
@@ -119,17 +119,28 @@ shift:
 	shl bx, 6
 	shr bx, 3		; 7. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutput		; Base32 Charakter ausgeben
+	call addOutputFirst	; Base32 Charakter ausgeben
 
 	;; 8. 5 bit
 	shl bx, 8
 	shr bx, 3		; 8. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutput		; Base32 Charakter ausgeben
+	call addOutputSecond	; Base32 Charakter ausgeben
 
 	
 	cmp rsi, 0		; Vergleicht ob noch Daten vorhanden sind.
 	jnz shift 		; Wenn noch Daten vorhanden sind springe zu Shift und beginne das Prozedere von vorne
+
+	
+addEqualSign:
+	mov rax, r11		; Divident: Counter für Anzahl Bytes in Rax kopieren
+	mov r13, 8		; Divisor: 8
+	div r13			; rax durch r13 = 8 rechnen
+	cmp rdx, 0		; Überprüfen ob die Division keinen Rest ergibt
+	jz write		; Wenn 0 dann sind es alles Achterblöcke
+	mov [output+r11], byte '=' ; EqualSign in die Ausgabe schreiben
+	inc r11			   ; Counter für Output erhöhen
+	jmp addEqualSign	   ; Wieder Überprüfen ob es ein EqualSign benötigt
 	
 
 write:
@@ -147,15 +158,19 @@ done:
 
 ; METHODEN
 
-addOutput:
-	mov [output+r9], cl 	; Ausgabe des Base32 Zeichen
+addOutputFirst:			; Für den ersten Durchlauf nach dem einlesen des Bits
+	mov [output+r11], cl 	; Ausgabe des Base32 Zeichen
+	inc r11			; counter für die Länge heraufzählen
+	ret			; Zurück zum Code
+
+addOutputSecond:	       ; Für den zweiten Durchlaufen nach dem einlesen des Bits
+	mov [output+r11], cl 	; Ausgabe des Base32 Zeichen
 	inc r11			; counter für die Länge heraufzählen
 	inc rsi			; rsi um 1 erhöhen für das nächste Byte
 	dec r10			; counter r10 runterzählen
 	cmp r10, 0 		; Vergleicht ob noch Daten vorhanden sind.
-	jz write		; Wenn keine Daten mehr vorhanden sind Springe zu Schreiben
+	jz addEqualSign		; Wenn keine Byte mehr vorhanden sind Springe zu addEqualSign
 	ret			; Zurück zum Code
-
 
 	
 
