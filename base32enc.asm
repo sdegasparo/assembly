70623cfe5a1594220e64b6a46c7144bcc83e4cda
zeroplexer <41778593+zeroplexer@users.noreply.github.com>
2018-12-01 18:40:41 +0100

    Add files via upload

--- base32enc.asm
+++ base32enc.asm
@@ -48,8 +48,20 @@ checkDone:
 	je done			; Springe zu Fertig, wenn rax 0 ist.
 
 ;;; checkShouldReadAnotherLine???
-	add rax, r10		 ;Input groesse zu rax addieren
+	add rax, r10		 ;Input groesse zu rax addieren BRAUCHTS DAS?
+	mov rbx, 8		 ; RBX = Mulitplikator (8)
+	mul rbx			 ; rax (Anzahl Byte) * rbx (8) = Anzahl Bit
+	mov rbx, 5		 ; rbx = Divisor (5)
+	div rbx			 ; rax (Anzahl Bit) / rbx (5) = Anzahl durchgänge
+	cmp rdx, 0		 ; Überprüfen auf Rest
+	jz noAddCounter
+
+	inc rax			; Wenn es Rest gab, dann rax um 1 erhöhen
+	
+noAddCounter:	
 	mov r10, rax		; r10 als counter verwenden
+	
+	
 	cmp byte [rsi+rax-1], 10 ; Vergleiche letzten Charakter mit neuer Zeile
 	jne prepareRegister	 ; Wenn der letzte Charakter vom Input kiene neue Zeile ist dann encoden
 
@@ -66,70 +78,84 @@ prepareRegister:
 	xor r11, r11		; Ausgabe groesse counter speichern
 
 T:				; Debug
+
+	
+
 	
 ;;; Verarbeiten
-shift:	
+	
+shift:
+	mov bh, [rsi]		; 1 Byte einlesen
 	;; 1. 5 bit
-	mov bx, [rsi]		; Byte 1 + 2 in bx kopieren
-	shr bx, 3		; 3 nach rechts shiften, damit die ersten 5 Bits verarbeitet werden. 1. Zeichen gefunden.
+	shr bx, 11		; 3 nach rechts shiften, damit die ersten 5 Bits verarbeitet werden. 1. Zeichen gefunden	
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutputFirst	; Base32 Charakter ausgeben
-	
-	
+	call addOutput		; Base32 Charakter ausgeben
+		
 	;; 2. 5 bit
-	shl bx, 8		;
-	shr bx, 3		; 2. Zeichen gefunden
+	mov bh, [rsi]	     ; 1. Byte wieder einlesen
+	inc rsi			; rsi um 1 erhöhen für das nächste Byte
+	mov bl, [rsi]		; 2 Byte in bl einlesen
+	shl bx, 5		;
+	shr bx, 11		; 2. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutputSecond	; Base32 Charakter ausgeben
+	call addOutput		; Base32 Charakter ausgeben
 
-	;; Byte 2 + 3 einlesen
-	mov bx, [rsi]		; Byte 2 + 3 in bx kopieren
-
+	;; Byte 2 einlesen
+	mov bh, [rsi]		; Byte 2 in bh einlesen
+	
 	;; 3. 5 bit
 	shl bx, 2
-	shr bx, 3		; 3. Zeichen gefunden
+	shr bx, 11		; 3. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutputFirst	; Base32 Charakter ausgeben
+	call addOutput		; Base32 Charakter ausgeben
 
 	;; 4. 5 bit
-	shl bx, 8
-	shr bx, 3		; 4. Zeichen gefunden
+	mov bh, [rsi]	     ; 2. Byte wieder einlesen
+	inc rsi			; rsi um 1 erhöhen für das nächste Byte
+	mov bl, [rsi]		; 3 Byte in bl einlesen
+	shl bx, 7
+	shr bx, 11		; 4. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutputSecond	; Base32 Charakter ausgeben
+	call addOutput		; Base32 Charakter ausgeben
 
 	;; Byte 3 + 4 einlesen
-	mov bx, [rsi]		; Byte 3 + 4 einlesen
+	mov bh, [rsi]		; Byte 3 wieder einlesen
+	inc rsi			; rsi um 1 erhöhen für das nächste Byte
+	mov bl, [rsi]		; Byte 4 einlesen
 
 	;; 5. 5 bit
 	shl bx, 4
-	shr bx, 3		; 5. Zeichen gefunden
+	shr bx, 11		; 5. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutputFirst	; Base32 Charakter ausgeben
+	call addOutput		; Base32 Charakter ausgeben
 
 	;; 6. 5 bit
-	shl bx, 8
-	shr bx, 3		; 6. Zeichen gefunden
+	mov bh, [rsi]		; Byte 4 wieder einlesen
+	shl bx, 1
+	shr bx, 11		; 6. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutputSecond	; Base32 Charakter ausgeben
+	call addOutput		; Base32 Charakter ausgeben
 
 	;; Byte 4 + 5 einlesen
-	mov bx, [rsi]		; Byte 4 + 5 einlesen
+	mov bh, [rsi]		; Byte 4 einlesen
+	inc rsi			; rsi um 1 erhöhen für das nächste Byte
+	mov bl, [rsi]		; Byte 5 einlesen
 
 	;; 7. 5 bit
 	shl bx, 6
-	shr bx, 3		; 7. Zeichen gefunden
+	shr bx, 11		; 7. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutputFirst	; Base32 Charakter ausgeben
+	call addOutput		; Base32 Charakter ausgeben
 
 	;; 8. 5 bit
-	shl bx, 8
-	shr bx, 3		; 8. Zeichen gefunden
+	mov bh, [rsi]		; Byte 5 wieder einlesen
+	shl bx, 3
+	shr bx, 11		; 8. Zeichen gefunden
 	mov cl, [Base32+ebx]	; Verwandle Eingabe in Base32 Charakter um
-	call addOutputSecond	; Base32 Charakter ausgeben
+	call addOutput		; Base32 Charakter ausgeben
 
-	
-	cmp rsi, 0		; Vergleicht ob noch Daten vorhanden sind.
-	jnz shift 		; Wenn noch Daten vorhanden sind springe zu Shift und beginne das Prozedere von vorne
+	inc rsi			; rsi um 1 erhöhen für das nächste Byte
+	jmp shift 		; Wenn noch Daten vorhanden sind springe zu Shift und beginne das Prozedere von vorne
 
 	
 addEqualSign:
@@ -158,15 +184,9 @@ done:
 
 ; METHODEN
 
-addOutputFirst:			; Für den ersten Durchlauf nach dem einlesen des Bits
+addOutput:	      		
 	mov [output+r11], cl 	; Ausgabe des Base32 Zeichen
 	inc r11			; counter für die Länge heraufzählen
-	ret			; Zurück zum Code
-
-addOutputSecond:	       ; Für den zweiten Durchlaufen nach dem einlesen des Bits
-	mov [output+r11], cl 	; Ausgabe des Base32 Zeichen
-	inc r11			; counter für die Länge heraufzählen
-	inc rsi			; rsi um 1 erhöhen für das nächste Byte
 	dec r10			; counter r10 runterzählen
 	cmp r10, 0 		; Vergleicht ob noch Daten vorhanden sind.
 	jz addEqualSign		; Wenn keine Byte mehr vorhanden sind Springe zu addEqualSign
